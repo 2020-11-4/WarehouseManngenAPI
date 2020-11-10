157782a88ea0ae483c5c5a73c029db19dd1a543c
HP <HP@DESKTOP-HNEDNFK>
2020-11-10 15:23:59 +0800

    API

---
+++ WMSAPI/WMSAPI/Controllers/WMSControllers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WMSAPI.Dal;
+
+namespace WMSAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WMSControllers : ControllerBase
+    {
+        private IWMSS _Wmss;
+        public WMSControllers(IWMSS wmss) 
+        {
+            _Wmss = wmss;
+        }
+    }
+}
---
+++ WMSAPI/WMSAPI/Dal/WMS.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMSAPI.Dal
+{
+    public class WMS:IWMSS
+    {
+        private readonly string _connectionString;
+        public WMS(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ZCY");
+        }
+
+
+    }
+}
