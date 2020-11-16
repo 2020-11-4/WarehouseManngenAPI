93f69b38f595f2371a689b03ea254a0d0375d6f8
快乐三代 <@YANGHAO>
2020-11-16 15:30:37 +0800

    1

--- WMSAPI/WMSAPI/Model/ckmx.cs
+++ WMSAPI/WMSAPI/Model/ckmx.cs
@@ -8,49 +8,36 @@ namespace WMSAPI.Model
 {
     public class ckmx
     {
-        public class product
-        {
-            /// <summary>
-            /// 产品表
-            /// </summary>
-            public int Product_Id { get; set; } //主键
+        /// <summary>
+        /// 产品表
+        /// </summary>
+        public int Product_Id { get; set; } //主键
 
-            public string PCategory { get; set; } //品类
+        public string PCategory { get; set; } //品类
 
-            public string Pcoding { get; set; } //产品编码
+        public string Pcoding { get; set; } //产品编码
 
-            public string PName { get; set; } //产品名称
+        public string PName { get; set; } //产品名称
 
-            public string Pspecification { get; set; } //产品规格
+        public string Pspecification { get; set; } //产品规格
 
-            public string Punit { get; set; } //计量单位
+        public string Punit { get; set; } //计量单位
 
-            public int Pamount { get; set; } //待入库量
+        public int Pamount { get; set; } //待入库量
 
-            public int Pgoods { get; set; } //关联仓库表
-        }
+        public int Pgoods { get; set; } //关联仓库表
         /// <summary>
         /// 货物表
         /// </summary>
-        public class Goods
-        {
-            [Key]
-            public int Gid { get; set; }//主键Id
-            public int Id { get; set; }//仓库id
-            public string Rsesrvoirare { get; set; }//库区
-            public string DescriptionGoods { get; set; }//仓库代码
-        }
+        public int Gid { get; set; }//主键Id
+        public int Id { get; set; }//仓库id
+        public string Rsesrvoirare { get; set; }//库区
+        public string DescriptionGoods { get; set; }//仓库代码
         /// <summary>
         /// 供应商
         /// </summary>
-        public class Supplierss
-        {
-            [Key]
-            public int Sid { get; set; }//供应商id
-            [StringLength(30)]
-            public string Snumber { get; set; }//供应商编号
-            [StringLength(20)]
-            public string SName { get; set; }//供应商名称
-        }
+        public int Sid { get; set; }//供应商id
+        public string Snumber { get; set; }//供应商编号
+        public string SName { get; set; }//供应商名称
     }
 }
